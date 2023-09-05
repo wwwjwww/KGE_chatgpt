d7c99950f6d7647cdba5061b303d85d3970f30ec
wwwjwww <buptmywjw@163.com>
2023-09-05 03:53:57 +0000

    batch

--- llama_embed.py
+++ llama_embed.py
@@ -1,23 +1,81 @@
 # Load model directly
 import torch
 from transformers import LlamaForCausalLM, LlamaTokenizer
+import random
+
 
 tokenizer = LlamaTokenizer.from_pretrained("decapoda-research/llama-7b-hf")
 model = LlamaForCausalLM.from_pretrained("decapoda-research/llama-7b-hf")
-
+model = model.bfloat16()
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-relation_lis = []
-with open('./data/FB15k-237/relation2text.txt') as f:
-    for line in f:
-        relation_dic = {"relation": None, "embed": None}
-        relation = line.split('\t')
-        relation_trans = str(relation[1])
-        inputs = tokenizer(relation_trans, return_tensors='pt').to(device)
-        model = model.to(device)
-        embed = model.generate(inputs.input_ids, max_length=32, output_hidden_states=True)
-        relation_dic["relation"] = relation[0]
-        relation_dic["embed"] = embed
-        relation_lis.append(relation_dic)
-relation_file = open('relation_embed.pickle','wb')
-pickle.dump(relation_lis, relation_file)
-relation_file.close()
+
+def get_data_batch(inputs, batch_size=None, shuffle=False):
+    '''
+    循环产生批量数据batch
+    :param inputs: list类型数据，多个list,请[list0,list1,...]
+    :param batch_size: batch大小
+    :param shuffle: 是否打乱inputs数据
+    :return: 返回一个batch数据
+    '''
+    # rows,cols=inputs.shape
+    rows = len(inputs)
+    indices = list(range(rows))
+    if shuffle:
+        random.seed(100)
+        random.shuffle(indices)
+    while True:
+        batch_indices = indices[0:batch_size]  # 产生一个batch的index
+        indices = indices[batch_size:] + indices[:batch_size]  # 循环移位，以便产生下一个batch
+        batch_data = []
+        temp_data = find_list(batch_indices, inputs)
+        batch_data.append(temp_data)
+        yield batch_data
+
+def find_list(indices, data):
+    out = []
+    for i in indices:
+        out.append(data[i])
+    return out
+
+def get_next_batch(batch):
+    return batch.__next__()
+
+def load_relation_file(filename):
+    relation_lis = []
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            relation_dic = {"relation": None, "sentence": None}
+            relation = line.rstrip().split('\t')
+            relation_dic["relation"] = str(relation[0])
+            relation_dic["sentence"] = str(relation[1])
+            relation_lis.append(relation_dic)
+    return relation_lis
+
+if __name__ == '__main__':
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    relation_lis = []
+    file1 = './data/FB15k-237/relation2text.txt'
+    relation_lis = load_relation_file(file1)
+
+    iter = 5
+    batch = get_data_batch(inputs=relation_lis, batch_size=5, shuffle=True)
+
+    embed_lis = []
+    for i in range(iter):
+        embed_dic = {"relation": None, "embed": None}
+        batch_relation = get_next_batch(batch)
+        rela_dic = batch_relation[0]
+        for j in rela_dic:
+            print(j)
+            rela_sentence = j["sentence"]
+            inputs = tokenizer(j, return_tensors='pt').to(device)
+            model = model.to(device)
+            embed = model.generate(inputs.input_ids, max_length=64, output_hidden_states=True)
+            embed_dic["relation"] = j["relation"]
+            embed_dic["embed"] = embed
+            embed_lis.append(relation_dic)
+
+    relation_file = open('relation_embed.pickle','wb')
+    pickle.dump(embed_lis, relation_file)
+    relation_file.close()
